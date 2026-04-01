cdce269a7d36c74eff48a556f872d90375703704
kauzue <88610807+kauzue@users.noreply.github.com>
2026-04-01 23:55:47 +0900

    Add files via upload

---
+++ matching-app/src/main/kotlin/com/likelionsmuc/matching_app/MatchingAppApplication.kt
@@ -0,0 +1,11 @@
+package com.likelionsmuc.matching_app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MatchingAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<MatchingAppApplication>(*args)
+}
